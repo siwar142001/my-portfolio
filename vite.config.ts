bd70ce14d406486d5db9d07f41253089df442972
siwar142001 <selmisiwar3@gmail.com>
2026-07-12 22:55:50 +0200

    fix deployement

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/my-portfolio/' : '/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react()],
 }))
